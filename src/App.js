cf9ad370db411a30348bf658db315170a297e6c7
RichardT74 <richtrist@gmail.com>
2019-11-26 19:21:38 +0000

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
-import ContactsContainer from './components/ContactsContainer';
+import Contacts from './components/contacts';
+import Form from './Form';
 
 class App extends Component {
-    render() {
-        return (
-          <ContactsContainer/>
-        );
-    }
+  render() {
+      return (
+        <div className="container">
+          <Contacts contacts={this.state.contacts} />
+          <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+        </div>
+      )
   }
+
+  state = {
+      contacts: []
+  };
+
+  componentDidMount() {
+      fetch('http://localhost:3000/posts/')
+          .then(res => res.json())
+          .then((data) => {
+              this.setState({ contacts: data })
+          })
+          .catch(console.log)
+  }
+}
 
 export default App;
